26be04ebefc82c47d45012bcaafaaa5200f39650
achrag-works <contact@achrag.sbs>
2026-06-05 18:49:49 +0900

    fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://works.achrag.sbs',
+  devToolbar: { enabled: false },
+  vite: {
+    build: { assetsInlineLimit: 0 }
+  }
 });
